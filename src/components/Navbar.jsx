26e8db4173113e87ab6b04194e7d805c984dd031
AlamRazek <ohidul933@gmail.com>
2024-03-24 02:25:48 +0000

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
-import styled from "@emotion/styled";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
 import React from "react";
 import Pets from "@mui/icons-material/Pets";
+import { Mail } from "@mui/icons-material";
 
-const styledToolbar = styled(Toolbar)({
+const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
 
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+const Icons = styled(Box)(({ theme }) => ({}));
+
 const Navbar = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
+      <StyledToolbar>
         <Typography
           variant="h6"
           sx={{
@@ -20,8 +37,17 @@ const Navbar = () => {
         >
           Nama Dev
         </Typography>
-        <Pets />
-      </Toolbar>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Avatar src="https://images.pexels.com/photos/20508966/pexels-photo-20508966/free-photo-of-man-sitting-in-eyeglasses-and-t-shirt.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        </Icons>
+      </StyledToolbar>
     </AppBar>
   );
 };
